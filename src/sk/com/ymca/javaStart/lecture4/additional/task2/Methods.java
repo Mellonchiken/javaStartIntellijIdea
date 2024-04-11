4afb61c8c12649687799a40ed895370b6c5657ef
Oleksandr <78863703+verhovcev214265@users.noreply.github.com>
2024-04-11 15:16:04 +0200

    Feedback

--- src/sk/com/ymca/javaStart/lecture4/additional/task2/Methods.java
+++ src/sk/com/ymca/javaStart/lecture4/additional/task2/Methods.java
@@ -1,6 +1,8 @@
 package sk.com.ymca.javaStart.lecture4.additional.task2;
 
+// Name of the class should be more concrete
 public class Methods {
+    
     public double square(double r, double h) {
         return  2 * Math.PI * r * (r + h);
     }
